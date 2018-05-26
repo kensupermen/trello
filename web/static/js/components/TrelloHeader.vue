96f2268f894a61bb889851608c486af3419b7584
kensupermen <votantho93@gmail.com>
2018-05-26 10:32:13 +0700

    styling logo

--- web/static/js/components/TrelloHeader.vue
+++ web/static/js/components/TrelloHeader.vue
@@ -1,8 +1,11 @@
 <template>
-  <header class="container-fluid tl-navbar">
-    <div class="container">
-      Trello Logo
-      <router-link to="/login">Login</router-link>
+<header class="container-fluid tl-navbar">
+  <div class="container">
+    <img alt="logo" src="./images/logo.png" class="logo"/>
+    <span>Trello</span>
+    <div class="right-side">
+      <router-link to="/login" >Login</router-link>
+    </div>
     </div>
   </header>
 </template>
